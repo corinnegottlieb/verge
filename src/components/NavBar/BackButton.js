c35776f2b1d302abc9e431279cc8a974e2953c8c
roykesh <42666643+roysh13@users.noreply.github.com>
2019-01-29 19:43:13 +0200

    more buttons and context menu

--- src/components/NavBar/BackButton.js
+++ src/components/NavBar/BackButton.js
@@ -9,3 +9,8 @@ export default class BackButton extends Component {
     )
   }
 }
+
+/*
+To do
+- button functionality
+*/
--- src/components/NavBar/SearchBar.js
+++ src/components/NavBar/SearchBar.js
@@ -10,3 +10,8 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+/*
+To do
+- button functionality
+*/
