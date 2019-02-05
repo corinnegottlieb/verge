2bfe6216c49c3fb89349d72338ee6fb77e99e3bc
Guy Mazinter <gmazinter@gmail.com>
2019-02-05 15:09:14 +0200

    reverted to TORView plus some other changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { observer } from 'mobx-react'
 import NavBar from './components/NavBar/Navbar';
 import Topic from './components/TreeOfResults/Topic';
+import TORView from './components/TreeOfResults/TORView';
 import TORList from './components/TORList'
 import WikiView from './components/TreeOfResults/ContextMenu/WikiView';
 import Rank from './components/TreeOfResults/ContextMenu/Rank';
@@ -17,7 +18,8 @@ class App extends Component {
       <Router>
         <div className="App">
           <NavBar />
-          <Route path="/TOR" exact component={Topic} />
+          {/* <Route path="/TOR" exact component={Topic} /> */}
+          <Route path="/TOR" exact component={TORView} />
           <Route path="/TORList" exact component={TORList} />
         </div>
       </Router>
---
+++ src/components/TreeOfResults/TORView.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
+import ContextMenu from './ContextMenu/ContextMenu';
+import TrackTOR from './TrackTOR'
+import Check from './Check';
+
+@inject("lumberYard")
+@observer
+class TORView extends Component {
+    toggleMenu = (event) => {
+        this.props.lumberYard.findTopicByName(event.target.id, this.props.lumberYard.toggleMenu)
+    }
+    buildHTMLTree = (cTOR) => {
+        return (
+            <div>
+                <div className={`level${cTOR.level} singleTopic`}
+                    onMouseEnter={this.toggleMenu} onMouseLeave={this.toggleMenu} id={cTOR.name}>
+                    <Check name={cTOR.name}/>
+                    {cTOR.name}
+                    {cTOR.menu ? <ContextMenu cTOR={cTOR} /> : null}
+                </div>
+                {cTOR.children ? 
+                    cTOR.children.map(c => 
+                    <div>{this.buildHTMLTree(c)}</div>
+                    ) :
+                    null}
+            </div>
+        )
+    }
+    render() {
+        return (
+            <div className="TORContainer">
+                {this.props.currentTOR ?
+                    null :
+                    <TrackTOR />}
+                {this.buildHTMLTree(this.props.lumberYard.currentTOR)}
+            </div>
+        )
+    }
+}
+export default TORView;
