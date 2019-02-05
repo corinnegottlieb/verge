dbe2eb423ed5a8625540dc876e50c2cdd2d14816
roykesh <42666643+roysh13@users.noreply.github.com>
2019-02-05 22:52:38 +0200

    icons, note, wiki

--- src/components/NavBar/SearchBar.js
+++ src/components/NavBar/SearchBar.js
@@ -17,20 +17,23 @@ class SearchBar extends Component {
 
   enterToSend = (e) => {
     if (e.key === 'Enter') {
-      this.sendSearch()
+      this.sendSearch();
+      return <Link to='/TOR'></Link>
     }
   }
 
   render() {
     return (
+      // <Link to='/TOR'>
       <div>
         <input type="search"
           value={this.props.lumberYard.searchValue}
           placeholder="search" onChange={this.handleSearchInput}
           onKeyDown={this.enterToSend}></input>
-        <Link to='/TOR'> <button onClick={this.sendSearch}
-          className="brown-text text-darken-4 btn waves-effect waves-light #4db6ac teal lighten-2">verge to search</button></Link>
+        <button onClick={this.sendSearch}
+          className="brown-text text-darken-4 btn-large waves-effect waves-light #4db6ac teal lighten-2">verge to search</button>
       </div>
+      // </Link>
     )
   }
 }
--- src/components/TreeOfResults/ContextMenu/Remove.js
+++ src/components/TreeOfResults/ContextMenu/Remove.js
@@ -5,13 +5,16 @@ import { observer, inject } from 'mobx-react';
 @observer
 class Remove extends Component {
 
-  remove=(event)=>{
+  remove = (event) => {
     this.props.lumberYard.findTopicByNameAndRemove(event.target.name)
     this.props.lumberYard.updateTOR()
   }
   render() {
     return (
-      <button name={this.props.name} onClick={this.remove}>Remove Topic</button>
+      <button className="open-note-button brown-text text-darken-4 btn-small waves-effect waves-light #4db6ac teal lighten-2"
+        name={this.props.name} onClick={this.remove}>
+        <i className="large material-icons">clear</i>
+      </button>
     )
   }
 }
