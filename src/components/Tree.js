9d88b36f06d5423ebe3e71efcd7f3698f4ebe29c
corinnegottleb <corinneottlieb@gmail.com>
2019-02-06 00:40:33 +0200

    created route to display tree graph

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -4,18 +4,18 @@ import { observer, inject } from 'mobx-react';
  
 @inject("lumberYard")
 @observer
-class MyComponent extends React.Component {
+class TreeGraph extends React.Component {
   render() {
     const data = [this.props.lumberYard.currentTOR]
 
     return (
-      <div id="treeWrapper" style={{width: '100vw', height: '100vh', marginTop: '50px'}}>
+      <div id="treeWrapper" style={{width: '100vw', height: '100vh', margin: '50px'}}>
  
-        <Tree data={data} separation={{siblings: 0.2, nonSiblings: 0.5}} orientation={`vertical`} />
+        <Tree data={data} separation={{siblings: 0.2, nonSiblings: 0.5}} orientation={`vertical`} translate={{x: 614, y: 73}} textLayout={{textAnchor: "start", x: 10, y: 20, transform:"skewY(30)" }}/>
  
       </div>
     );
   }
 }
 
-export default MyComponent
+export default TreeGraph
--- src/components/TreeOfResults/ContextMenu/ContextMenu.js
+++ src/components/TreeOfResults/ContextMenu/ContextMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import OpenNote from './OpenNote';
 import Remove from './Remove';
 import WikiView from './WikiView';
+import { Link } from 'react-router-dom'
 
 class ContextMenu extends Component {
 
@@ -11,6 +12,9 @@ class ContextMenu extends Component {
                 <OpenNote cTOR={this.props.cTOR} />
                 <WikiView currentTopic={this.props.cTOR} />
                 <Remove name={this.props.cTOR.name} />
+                <Link to='./tree'>
+                    <button>View as graph</button>
+                </Link>
             </div>
         )
     }
