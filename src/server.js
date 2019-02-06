7f6f16f2ef56283e241f4425f039a921246ebd57
corinnegottleb <corinneottlieb@gmail.com>
2019-02-06 21:36:37 +0200

    removed build code

--- src/server.js
+++ src/server.js
@@ -7,12 +7,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 //this needs to be changed before deployment:
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    next()
-})
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+//     next()
+// })
 
 const mongoose = require('mongoose')
 // mongoose.connect("mongodb://localhost/VergeDB")
