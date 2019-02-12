fbd67930712136dc41f92211d8812423d56c46b2
roykesh <42666643+roysh13@users.noreply.github.com>
2019-02-12 12:49:21 +0200

    added authorisation for irrelevance and TrackTOR

--- src/components/TreeOfResults/TrackTOR.js
+++ src/components/TreeOfResults/TrackTOR.js
@@ -5,8 +5,13 @@ import { observer, inject } from 'mobx-react';
 @observer
 class TrackTOR extends Component {
   toggleTracked = () => {
-    this.props.lumberYard.toggleTracked()
+    if (this.props.lumberYard.currentTOR.tracked && window.confirm('Are you sure you want to stop tracking this topic?')) {
+      this.props.lumberYard.toggleTracked()
+    } else if (!this.props.lumberYard.currentTOR.tracked) {
+      this.props.lumberYard.toggleTracked()
+    }
   }
+
   render() {
     return (
       <div className={`pin${this.props.lumberYard.currentTOR.tracked}`} onClick={this.toggleTracked}>
